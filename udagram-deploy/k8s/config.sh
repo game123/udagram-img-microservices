30b57f5282a25de87190b46870813b53bed1c610
Kevin Chan <kevinykchan@gmail.com>
2020-06-18 20:06:52 -0400

    updated yaml files

---
+++ udagram-deploy/k8s/config.sh
@@ -0,0 +1,22 @@
+# echo -n $POSTGRESS_USERNAME | base64
+# echo -n $POSTGRESS_PASSWORD | base64
+# base64 .aws/credentials
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+kubectl apply -f backend-feed-deployment.yaml 
+
+kubectl apply -f backend-feed-service.yaml 
+
+kubectl apply -f backend-user-deployment.yaml 
+
+kubectl apply -f backend-user-service.yaml 
+
+kubectl apply -f frontend-deployment.yaml  
+
+kubectl apply -f frontend-service.yaml 
+
+kubectl apply -f reverseproxy-deployment.yaml 
+
+kubectl apply -f reverseproxy-service.yaml 
